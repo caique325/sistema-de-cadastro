61f3e277a0a9ce177e2d90c01c4fd755f4192ee0
caique325 <caique-magalhaes1@outlook.com>
2023-10-04 16:42:39 -0300

    voltando atras

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log('Rodando o sistemas de gerenciamento de jogos')
 console.log('aplicacao esta se conectando ao banco de dados')
 console.log('O paulo fez esse commit sem vc ver na aula')
-console.log('Enviando para o Github')
