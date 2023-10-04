6fb2fcb3705a3eb85a4a5c27fb49045fc1ad275e
caique325 <134814288+caique325@users.noreply.github.com>
2023-10-04 17:07:10 -0300

    add alert app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log('Rodando o sistemas de gerenciamento de jogos')
 console.log('aplicacao esta se conectando ao banco de dados')
 console.log('O paulo fez esse commit sem vc ver na aula')
+alert('ola mundo')
