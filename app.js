75653b062919f9f47c56a6be1580cad445bc489b
caique325 <caique-magalhaes1@outlook.com>
2023-10-05 17:33:44 -0300

    corrigindo app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log('Rodando o sistemas de gerenciamento de jogos')
 console.log('aplicacao esta se conectando ao banco de dados')
 console.log('O paulo fez esse commit sem vc ver na aula')
+console.log('status')
 
